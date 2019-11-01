f7026f900b5cb0fa6cee1546ec5e3c720e029140
zhylmzr <zhylmzr@gmail.com>
2019-11-01 13:42:41 +0800

    add: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "jest": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double",
+            { allowTemplateLiterals: true }
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- src/word-pinyin.ts
+++ src/word-pinyin.ts
@@ -2,7 +2,7 @@ import dictNotoneOrigin from "./dict/pinyin_dict_notone";
 import dictFirstletter from "./dict/pinyin_dict_firstletter";
 
 // 处理拼音字典
-const dictNotone: Record<string, string> = {}
+const dictNotone: Record<string, string> = {};
 for(var i in dictNotoneOrigin) {
     var temp = dictNotoneOrigin[i];
     for(var j=0, len=temp.length; j<len; j++) {
@@ -16,22 +16,22 @@ export default {
      * @param chinese 要转换的汉字
      * @param splitter 分隔字符，默认用空格分隔
      */
-    getPinyin: function (chinese: string, splitter: string = ' ') {
-        if (!chinese || /^ +$/g.test(chinese)) return '';
+    getPinyin: function (chinese: string, splitter: string = " ") {
+        if (!chinese || /^ +$/g.test(chinese)) return "";
         var result: string[] = [];
 
-        var noChinese = '';
+        var noChinese = "";
         for (var i = 0, len = chinese.length; i < len; i++) {
             var temp = chinese.charAt(i),
                 pinyin = dictNotone[temp];
             if (pinyin) { //插入拼音
                 //空格，把noChinese作为一个词插入
-                noChinese && (result.push(noChinese), noChinese = '');
+                noChinese && (result.push(noChinese), noChinese = "");
                 result.push(pinyin);
             }
             else if (!temp || /^ +$/g.test(temp)) {
                 //空格，插入之前的非中文字符
-                noChinese && (result.push(noChinese), noChinese = '');
+                noChinese && (result.push(noChinese), noChinese = "");
             }
             else {
                 //非空格，关联到noChinese中
@@ -41,7 +41,7 @@ export default {
 
         if (noChinese) {
             result.push(noChinese);
-            noChinese = '';
+            noChinese = "";
         }
         return result.join(splitter);
     },
@@ -50,7 +50,7 @@ export default {
      * @param str 汉字字符串，如果遇到非汉字则原样返回
      */
     getFirstLetter: function (str: string) {
-        if (!str || /^ +$/g.test(str)) return '';
+        if (!str || /^ +$/g.test(str)) return "";
 
         var result: string[] = [];
         for (var i = 0; i < str.length; i++) {
@@ -61,6 +61,6 @@ export default {
             }
             result.push(ch);
         }
-        return result.join(''); // 如果不用管多音字，直接将数组拼接成字符串
-    },
+        return result.join(""); // 如果不用管多音字，直接将数组拼接成字符串
+    }
 };
